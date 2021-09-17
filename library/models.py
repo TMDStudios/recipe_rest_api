109af9e4444c1301cbddb05a49019cc2c3c3a80b
TMDStudios <tmdstudioscontact@gmail.com>
2021-09-17 09:44:37 -0400

    Initial Commit

---
+++ library/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    ingredients = models.TextField(max_length=1080)
+    instructions = models.TextField(max_length=1080)
+
+    def __str__(self) -> str:
+        return self.title
---
+++ library/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .models import Recipe
+from .serializers import RecipeSerializer
+
+class RecipeList(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    # 36:58
+
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
